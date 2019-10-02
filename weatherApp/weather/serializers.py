868bfac050ee8135e8255b50f517fa43fbc9f75e
kaledqal <kalenshi@gmail.com>
2019-10-02 11:34:48 -0300

    Changes to be committed: 	modified:   .DS_Store 	modified:   dump.rdb 	modified:   weather/serializers.py 	modified:   weatherApp/urls.py

--- weatherApp/weather/serializers.py
+++ weatherApp/weather/serializers.py
@@ -8,6 +8,14 @@ __author__ = 'Kalenshi Katebe'
 from rest_framework import serializers
 
 
+def city_validator(city):
+    """
+    Validates the provided city
+    :param city:
+    :return:
+    """
+
+
 class WeatherSerializer(serializers.Serializer):
     """Serializes a city field for accessing the weather api"""
     city = serializers.CharField(max_length=50,
